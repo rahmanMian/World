7267ffbbc3e6f7f11f99c4f837f931adab239cd3
mahran12 <89672689+mahran12@users.noreply.github.com>
2023-12-22 07:37:02 -0500

    Can move code

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -4,5 +4,12 @@ class Segment{
         this.p2 = p2;
     }
 
-    draw(ctx, )
+    draw(ctx, width = 2, color ="black"){
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.strokeStyle = color;
+        ctx.moveTo(this.p1.x, this.p1.y);
+        ctx.lineTo(this.p2.x, this.p2.y);
+        ctx.stroke();
+    }
 }
