4af4b8f274a02e8e3e305f109570ee36d54751bd
mahran12 <89672689+mahran12@users.noreply.github.com>
2023-12-23 09:11:44 +0500

    gym break add movement fro trackpads too

--- js/viewport.js
+++ js/viewport.js
@@ -4,6 +4,13 @@ class Viewport{
     this.ctx = canvas.getContext("2d");
 
     this.zoom = 1;
+    this.offset = new Point(0, 0);
+    this.drag = {
+        start: new Point(0, 0),
+        end: new Point(0, 0),
+        offset: new Point(0, 0),
+        active: false
+    };
 
     this.#addEventListeners();
    }
@@ -17,7 +24,36 @@ class Viewport{
 
    #addEventListeners(){
     this.canvas.addEventListener("mousewheel", this.#handleMouseWheel.bind(this));
+    this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+    this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+    this.canvas.addEventListener("mouseup", this.#handleMouseUp.bind(this));
    }
+
+   #handleMouseDown(evt){
+    if (evt.button == 1){ 
+        this.drag.start = this.getMouse(evt);
+        this.drag.active = true;
+    }
+   }
+
+    #handleMouseMove(evt){
+        if (this.drag.active){
+            this.drag.end = this.getMouse(evt);
+            this.drag.offset = subtract(this.drag.end, this.drag.start);
+        }
+    }
+
+    #handleMouseUp(evt){
+        if (this.drag.active){
+            this.offset = add(this.offset, this.drag.offset);
+            this.drag = {
+                start: new Point(0, 0),
+                end: new Point(0, 0),
+                offset: new Point(0, 0),
+                active: false
+            };
+        }
+    }
 
    #handleMouseWheel(evt){
     const dir = Math.sign(evt.deltaY);
