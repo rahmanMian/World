59e3d5da9b218435d1de63b67c3aaa35fbc513c2
mahran12 <89672689+mahran12@users.noreply.github.com>
2023-12-26 09:32:23 +0500

    fixed dispose button issue

---
+++ js/math/graph.js
@@ -0,0 +1,97 @@
+class Graph{
+    constructor(points =[], segments = []){
+        this.points = points;
+        this.segments = segments;
+    }
+
+    static load(info) {
+        const points = [];
+        const segments = [];
+        for (const pointInfo of info.points){
+            points.push(new Point(pointInfo.x, pointInfo.y));
+        }
+
+        for (const segInfo of info.segments){
+            segments.push(new Segment
+         (points.find((p) => p.equals(segInfo.p1)),
+          points.find((p) => p.equals(segInfo.p2))
+          ));
+        }
+        return new Graph(points, segments);
+    }
+    
+    addPoint(point){
+        this.points.push(point);
+    }
+
+    containsPoint(point){
+        return this.points.find((p) => p.equals(point));
+    }
+
+    tryAddPoint(point){
+        if(!this.containsPoint(point)){
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
+
+    removePoint(point){
+        const segs = this.getSegmentsWithPoint(point);
+        for(const seg of segs){
+            this.removeSegment(seg);
+        }
+        this.points.splice(this.points.indexOf(point), 1);
+       }
+
+    addSegment(seg){
+        this.segments.push(seg);
+    }
+
+   
+    containsSegment(seg){
+        return this.segments.find((s) => s.equals(seg));
+    }
+
+   tryAddSegment(seg){
+    if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)){
+        this.addSegment(seg);
+        return true;
+    }
+     return false;
+   }
+    
+   removeSegment(seg){
+    this.segments.splice(this.segments.indexOf(seg), 1);
+   }
+ 
+   getSegmentsWithPoint(point){
+    const segs = [];
+    for(const seg of this.segments){
+        if(seg.includes(point)){
+            segs.push(seg);
+        }
+    }
+    return segs;
+   }
+
+
+   dispose(){
+    this.points.length = 0;
+    this.segments.length = 0;
+   }
+
+    //points = nodes, segments = edges
+    draw(ctx){
+        for(const seg of this.segments){
+            seg.draw(ctx);
+        }
+        //draw points after to make look better
+        for(const points of this.points){
+            points.draw(ctx);
+        }
+    }
+
+
+}
---
+++ js/math/utils.js
@@ -0,0 +1,40 @@
+function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER){
+    let minDist = Number.MAX_SAFE_INTEGER;
+    let nearest = null;
+    for(const point of points){
+        const dist = distance(point, loc);
+        if(dist < minDist && dist < threshold){
+            minDist = dist;
+            nearest = point;
+        }
+    }
+    return nearest;
+}
+
+function distance(p1, p2){
+    return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+}
+
+function add(p1, p2){
+    return new Point(p1.x + p2.x, p1.y + p2.y);
+}
+
+function subtract(p1, p2){
+    return new Point(p1.x - p2.x, p1.y - p2.y);
+}
+
+
+function scale(p , scalar) {
+    return new Point(p.x * scalar, p.y * scalar);
+}
+
+function translate(loc, angle, offset){
+    return new Point(
+        loc.x + Math.cos(angle) * offset,
+        loc.y + Math.sin(angle) * offset
+    );
+}
+
+function angle(p){
+    return Math.atan2(p.y, p.x);
+}
